85ba03d4e0e469fe52a51b4e4fd3b291c5d09688
unknown <juliana_guonik@hotmail.com>
2022-05-28 11:17:34 -0300

    Projeto la-vie com TS, DDD e SOLID

---
+++ la-vie-ts/dist/externals/viaCep/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.viaCepApi = void 0;
+const ViaCep_1 = require("./ViaCep");
+exports.viaCepApi = new ViaCep_1.ViaCep();
+// console.log(ViaCepApi.getAddress('01001000'));
---
+++ la-vie-ts/src/infrastructure/viaCep/index.ts
@@ -0,0 +1,4 @@
+import { ViaCep } from "./ViaCep";
+
+export const viaCepApi = new ViaCep();
+// console.log(ViaCepApi.getAddress('01001000'));
